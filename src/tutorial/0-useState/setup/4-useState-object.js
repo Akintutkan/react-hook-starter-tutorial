cadbf4a9ab018918b8f82b18612539a00533032c
Akin94 <akin.tutkan@gmail.com>
2022-12-04 19:41:55 +0300

    useState Counter Demo

--- src/tutorial/0-useState/setup/4-useState-object.js
+++ src/tutorial/0-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name,setName] = useState("murat")
+  const [age,setAge] = useState(36)
+  const [message,setMessage] = useState("rastgele mesaj")
+  const changeMessage =() =>{
+    //setPerson({...person, message : "hello world" })
+  setMessage("hello World")
+  
+  }
+  return (<>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className="btn" onClick={changeMessage}>
+    mesajı değiştir
+  </button>
+  </>
+  )
 };
 
 export default UseStateObject;
--- src/tutorial/0-useState/setup/5-useState-counter.js
+++ src/tutorial/0-useState/setup/5-useState-counter.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  //  const handleUpdate = () => {
+  //  setValue(value + 1)
+  //   console.log(value)
+  // }
+  const reset = () => {
+    setValue(0);
+  };
+  const karmaşıkArtış =()=>{
+    setTimeout(()=>{
+      // setValue(value +1)
+    setValue((p) => p + 1)
+    }, 2000)
+  }
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Normal Sayaç</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue((p) => p - 1)}>Azalt</button>
+        <button className="btn" onClick={reset}>Reset</button>
+        <button className="btn" onClick={() => setValue((p) => p + 1)}>Arttır</button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Karmaşık Sayaç</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={karmaşıkArtış}>Sonra Arttır</button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
