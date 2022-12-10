8e2a0b325a298f3d2e56e20fe3bc9cec6ea2c9b2
Akin94 <akin.tutkan@gmail.com>
2022-12-10 16:56:11 +0300

    useEffect FetchData Example

--- src/tutorial/1-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/1-useEffect/setup/2-useEffect-cleanup.js
@@ -5,17 +5,22 @@ import React, { useState, useEffect } from 'react'
 const UseEffectCleanup = () => {
   const [size,setSize]= useState(window.innerWidth)
   const checkSize = () =>{
-    setSize(window.innerwith)
+    setSize(window.innerWidth)
   }
   useEffect(() =>{
+    console.log("useeffect")
     window.addEventListener("resize", checkSize)
-  })
-  console.log(size)
+  // return () => {
+  //   console.log("cleanup")
+  //   window.removeEventListener("resize",checkSize)
+  // }
+  },[])
+   console.log("render")
   return (
   <>
   <h1>window</h1>
   <h2>{size} px</h2>
-    </>
+  </>
 )
 }
 
