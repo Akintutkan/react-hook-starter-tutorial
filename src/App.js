4e4c757bc1e6d2f59bbf1989d739786f8996a713
Akin94 <akin.tutkan@gmail.com>
2022-12-11 17:02:21 +0300

    useREf Example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from "./tutorial/3-forms/setup/2-multiple-inputs"
+import Setup from "./tutorial/4-useRef/setup/1-useRef-basics"
 function App() {
   return (
     <div className='container'>
