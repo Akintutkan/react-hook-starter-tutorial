5891593bca7e5d572fb01755d941c277612b405d
Akin94 <akin.tutkan@gmail.com>
2023-01-13 00:47:11 +0300

    Sabaha hazırlık

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Setup from "./tutorial/1-useEffect/setup/3-useEffect-fetch-data"
-import Setup from "./tutorial/0-useState/setup/5-useState-counter"
+import Setup from "./tutorial/1-useEffect/setup/1-useEffect-basics"
 function App() {
   return (
     <div className='container'>
