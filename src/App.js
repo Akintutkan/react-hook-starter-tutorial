c87fa5efed92da48748ba2f42d75c3cdd51989a0
Akin94 <akin.tutkan@gmail.com>
2023-01-12 23:58:28 +0300

    Açıklamalar eklendi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Setup from "./tutorial/1-useEffect/setup/3-useEffect-fetch-data"
-import Setup from "./tutorial/0-useState/setup/2-useState-basics"
+import Setup from "./tutorial/0-useState/setup/3-useState-array"
 function App() {
   return (
     <div className='container'>
--- src/tutorial/0-useState/setup/3-useState-array.js
+++ src/tutorial/0-useState/setup/3-useState-array.js
@@ -2,21 +2,20 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  const [people,setPeople] = React.useState(data)
+  const [people,setPeople] = React.useState(data) // import {useState} yazmadan Reacttan useState çağır yazdık
   const removeItem = (id) => {
-    setPeople((oldPeople) =>{
-      let newPeople = oldPeople.filter((person) => person.id !== id)
-    return newPeople
-    })
+    let newPeople = people.filter((person) => person.id !== id) // id'eşit olmayanları kaldır ! yerine = koysaydık bastığımızı tutardı
+    //remove yerine handle ederdi
+  setPeople(newPeople)  // sonra bunu set people ile çağır bu remove item fonksiyonunun aşağıda çağırdık
   }
   return (
   <>
   {people.map((person) => {
-    const {id,name} = person
+    const {id,name} = person // distructing useStateden aktarılan people maplendi ve people arrayi distructing yapıldı. 
     return( 
     <div key={id}  className="item">
       <h4> {name} </h4>
-      <button onClick = {() => removeItem(id)}> Kaldır</button>
+       <button onClick = {() => removeItem(id)}> Kaldır</button> {/*id'den yakalaması için burada içeri id çağırıldı */}
     </div>
     )
   })}
