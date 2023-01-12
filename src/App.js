9ba8a1d2752fb9979086d170fb1664cb0c7711b0
Akin94 <akin.tutkan@gmail.com>
2023-01-12 23:18:26 +0300

    Açıklamalar eklendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Setup from "./tutorial/1-useEffect/setup/3-useEffect-fetch-data"
+// import Setup from "./tutorial/1-useEffect/setup/3-useEffect-fetch-data"
+import Setup from "./tutorial/0-useState/setup/2-useState-basics"
 function App() {
   return (
     <div className='container'>
--- src/tutorial/0-useState/setup/2-useState-basics.js
+++ src/tutorial/0-useState/setup/2-useState-basics.js
@@ -5,12 +5,14 @@ const UseStateBasics = () => {
   // const değer = useState(1)[0]
   // const handler = useState(1)[1]
   // console.log(değer,handler)
-  const[text,setText]  = useState("Rastgele başlık")
+  const[text,setText]  = useState("Rastgele başlık") // useState Hooku 2 parametre alır biri durum diğeri de durumu değiştiren fonksiyon
+  // bu durum bir string bir sayı bir array olabilir yukarıdaki 5-6 satırlar gibi yazmaktansa 8 deki gibi yazabiliriz.
+  // Hooku 2. satırda yani fonksiyonundışında ya da bir koşul ifadesinin içerisinde çağıramayız
   const handleClick = () => {
    if(text === "Rastgele başlık") {
-    setText("selam Millet")
+    setText("selam Millet") //text rastgele başlıksa Selam millet yap
   }else{
-    setText("Rastgele başlık")
+    setText("Rastgele başlık") // değilse Rastgele başlık olarak değiştir
   } 
 }
   return (
