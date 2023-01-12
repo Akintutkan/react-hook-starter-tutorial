28b1a2359e8050df9b7064ff0cb8a39822102e3e
Akin94 <akin.tutkan@gmail.com>
2023-01-13 00:28:41 +0300

    açıklamalae eklendi

--- src/tutorial/0-useState/setup/4-useState-object.js
+++ src/tutorial/0-useState/setup/4-useState-object.js
@@ -4,13 +4,15 @@ const UseStateObject = () => {
   const [name,setName] = useState("murat")
   const [age,setAge] = useState(36)
   const [message,setMessage] = useState("rastgele mesaj")
+  //const [person,setPerson] = useState({name:"akın",age:28,message:" selamlar"}) tek bir objeyi bir state kullanarak saklayabiliriz
   const changeMessage =() =>{
-    //setPerson({...person, message : "hello world" })
+    //setPerson({...person, message : "hello world" }) ve eğer personda sadece message değiştirilecekse bu kullanılır diğer özellikler değişmeyecekse
+    //spread operatörü kullanılmalı
   setMessage("hello World")
   
   }
   return (<>
-  <h3>{name}</h3>
+   <h3>{name}</h3> {/*person objesini çağırmak için person.name person.age person.message kullanmalı */}
   <h3>{age}</h3>
   <h3>{message}</h3>
   <button className="btn" onClick={changeMessage}>
