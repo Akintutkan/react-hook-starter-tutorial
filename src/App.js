3ffbe346647dd035764b068fe41dfe98d04f6d45
Akin94 <akin.tutkan@gmail.com>
2023-01-12 22:34:32 +0300

    Konu tekrarlarında Açıklamalar girildi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from "./tutorial/10-memo-useMemo-useCallback/setup/index"
+import Setup from "./tutorial/1-useEffect/setup/3-useEffect-fetch-data"
 function App() {
   return (
     <div className='container'>
--- src/tutorial/0-useState/setup/1-error-example.js
+++ src/tutorial/0-useState/setup/1-error-example.js
@@ -1,14 +1,14 @@
 import {Fragment} from 'react';
 
-const ErrorExample = () => {
-  let title = "Rastgele başlık"
-  const handleClick = () => {
+const ErrorExample = () => { // Butona tıklanıldığında geri dönüş oluşturmak istiyoruz
+  let title = "Rastgele başlık" // ekranda olacak bir başlık oluşturuldu
+  const handleClick = () => { // bu fonksiyona değişmesi istenen başlık atandı
      title = "Selam millet"
-     console.log(title)
+     console.log(title) // değer burada değişir ama render etmez bu yüzden //* UseState Kullanmalıyız
   }
   return <Fragment>
     <h2>{title}</h2>
-    <button type="button" className="btn" onClick={handleClick}>
+    <button type="button" className="btn" onClick={handleClick}> {/* Butona Tıkladığımızda handleClick çalışsın */}
       başlığı Değiştir
     </button>
   </Fragment>
