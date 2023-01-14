44a1ec10d53afc5665297df925b659f777b48470
Akin94 <akin.tutkan@gmail.com>
2023-01-14 23:09:05 +0300

    Açıklamalar eklendi.

--- src/tutorial/1-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/1-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,22 +3,24 @@ import React, { useState, useEffect } from 'react'
 const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  const [users,setUsers] = useState ([])
-  const getUsers = async() =>{
-    const response = await fetch(url)
-    const users = await response.json()
+  const [users,setUsers] = useState ([]) //userlar için başalngıçta boş bir array ayarrlandı
+  const getUsers = async() =>{ // fetch çağrısı yapıldı.
+    const response = await fetch(url) // response değişkenine fetch url çekildi
+    const users = await response.json() // users'a da çekilen fetch ögeleri aktarıldı
     setUsers(users)
    // console.log(users)
   }
   useEffect(() =>{
     getUsers()
-  },[])
+  },[]) //Eğer dependency array eklenmezse buradaki kodda sonsuz döngü oluşur setuser getuser fonksiyonunda çalışır  o çalıştığı için tekrar set edilir get çalışır
+  //dependency eklendiğinde bileşen oluşturulduktan sonra useeffect çalışır.
   return (
   <>
   <h3>Github Users</h3>
-  <ul className='users'>{users.map((user) => {
-const {id,login,avatar_url,html_url} =user
-return <li key={id}>
+  <ul className='users'>{users.map((user) => { //users nesnesini mapladık 
+const {id,login,avatar_url,html_url} =user // bu nesneyi destructuring yaptık
+ return <li key={id}> 
+ {/* her zaman key ister key'e de id verilir */}
   <img src={avatar_url} alt={login} />
   <div>
     <h4>{login}</h4>
