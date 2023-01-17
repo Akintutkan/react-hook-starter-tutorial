7413ce390f2ef1ab7dbb3aa12480a6fdf6b14347
Akin94 <akin.tutkan@gmail.com>
2023-01-17 17:13:37 +0300

    Açıklamalar eklendi

--- src/tutorial/2-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/2-conditional-rendering/setup/2-short-circuit.js
@@ -5,15 +5,16 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
 const [text,setText] = useState("")
 const [isError,setIsError] = useState(false)
-// const firstValue = text || "hello World"
-// const secondValue = text && "Hello World"
+// const firstValue = text || "hello World" text false olursa first valueden
+// const secondValue = text && "Hello World" text true ise 2. değerden çıkar
 // console.log(secondValue)
 
   return (
   <>
-  <h1>{text || "Akın"}</h1>
+  <h1>{text || "Akın"}</h1> {/* text değeri falsy olduğu için değer akın olarak döner */}
   <button className="btn" onClick={() => setIsError((s)=> !s)}>Toggle Error</button>
-  {isError && <h1>Error...</h1>}
+  {/* true ise false false ise true'ya çevir yaptık setisError'ün içini eğer içine !isError koyulursada olur ama callback içerisine konulması mantıklı*/}
+  {isError && <h1>Error...</h1>} {/* if else kullanamazsın ternary kullanavbilirisin hata varsa Error ver*/ }
   {isError ?<p>Hata var...</p>
   :(
     <div>
